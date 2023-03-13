de0c97927fabe022918d0b567e58b454c1f35ee2
Sergey Rozhin <namesergeyrozhin@yandex.ru>
2023-03-14 01:46:47 +0300

    Add command send and get

--- bot_settings.py
+++ bot_settings.py
@@ -1,6 +1,9 @@
+'''Settings module'''
 import os
 
 DB_URI = os.environ.get('DB_CONNECT')
-TOKEN = os.environ.get('API_KEY')
-LANG = os.environ.get('DEFAULT_LOCALE')
-LOCAL_PATH = os.environ.get('LOCALIZATION_PATH')
+TELEGRAM_API_KEY = os.environ.get('API_KEY')
+DEFAULT_LOCALE = os.environ.get('DEFAULT_LOCALE')
+LOCALIZATION_PATH = os.environ.get('LOCALIZATION_PATH')
+USER_SECRETS_MAX = os.environ.get('USER_SECRETS_MAX', 150)
+SECRET_HASH_LENGTH = os.environ.get('SECRET_HASH_LENGTH', 30)
